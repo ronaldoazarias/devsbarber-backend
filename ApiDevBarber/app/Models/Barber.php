7426cee7232c1ca17a6903c510255d0a2035b625
Ronaldo Azarias <ronaldo.azarias01@gmail.com>
2021-03-22 11:00:08 -0300

    ...

---
+++ ApiDevBarber/app/Models/Barber.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Barber extends Model
+{
+    use HasFactory;
+
+    //protected $table = 'barber';
+    public $timestamps = false;    
+}
